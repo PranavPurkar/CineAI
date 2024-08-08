6de9ae8edbdd84e33157127c6bb0eac82494e3d7
PranavPurkar <pranav.purkar07@gmail.com>
2024-08-08 23:59:17 +0530

    working on browse page!

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,22 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Header from './Header'
-import { API_OPTIONS } from '../Utils/constants'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import Maincontainer from './Maincontainer';
+import SecondaryContainer from './SecondaryContainer';
+
 
 const Browse = () => {
  
-  const getNowplayingMovies = async() => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
-    const json = await data.json();
-    console.log(json.results);
-  };
-
-  useEffect(() => {
-    getNowplayingMovies();
-  }, []);
+  useNowPlayingMovies();
 
   return (
     <div>
       <Header/>
+      <Maincontainer/>
+      <SecondaryContainer/>
     </div>
   )
 }
--- src/Utils/appstore.js
+++ src/Utils/appstore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import moviesReducer from "./movieSlice"; 
 
 
 const appstore = configureStore (
     {
         reducer: {
             user : userReducer,
+            movies : moviesReducer,
         },
     }
 );
